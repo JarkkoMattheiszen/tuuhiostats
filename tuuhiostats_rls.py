aec26edbe7b96c517bbcc793fd56d68901793b65
Jarkko Mattheiszen <44687760+JarkkoMattheiszen@users.noreply.github.com>
2022-08-11 20:02:12 +0300

    Add files via upload

--- tuuhiostats_rls.py
+++ tuuhiostats_rls.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
-# The script downloads PUBG season statistics for a range of 1-10 players, parses and sorts them and
-# outputs them to individual files to be used with Nightbot's urlfetch function
+
+# Tuuhiostats downloads PUBG season statistics for a range of 1-10 players, parses and sorts them and
+# outputs them to individuals files to be used with Nightbot's urlfetch function
 #
 # Use !commands add !command $(urlfetch https://yourserver/path/file) with Nightbot
 #
-# Copyright (C) sun muut semmoset BaronVonKale herran vuonna 2022, terveisiä kotiin
+# Copyright (C) ja sen semmoset herran vuonna 2022 BaronVonKale, terveisiä kotiin
 
 from __future__ import print_function
 import json
@@ -33,96 +33,106 @@ resp = requests.get(url, headers=headers)
 data = resp.json()
 
 # Get wins in the last 24 hours for the streamers
-winsStreamer1 = data['data'][0]['attributes']['gameModeStats']['squad-fpp']['dailyWins']
-winsStreamer2 = data['data'][1]['attributes']['gameModeStats']['squad-fpp']['dailyWins']
-winsStreamer3 = data['data'][2]['attributes']['gameModeStats']['squad-fpp']['dailyWins']
+wins_streamer1 = data['data'][0]['attributes']['gameModeStats']['squad-fpp']['dailyWins']
+wins_streamer2 = data['data'][1]['attributes']['gameModeStats']['squad-fpp']['dailyWins']
+wins_streamer3 = data['data'][2]['attributes']['gameModeStats']['squad-fpp']['dailyWins']
 
-# Define the players, input the player names as the last value
+# Define the players
 players = {
-    1 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player1' },
-    2 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player2' },
-    3 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player3' },
-    4 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player4' },
-    5 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player5' },
-    6 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player6' },
-    7 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player7' },
-    8 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player8' },
-    9 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player9' },
-    10 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'name' : 'player10' }
+    1 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'streamer1' },
+    2 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'streamer2' },
+    3 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'streamer3' },
+    4 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player4' },
+    5 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player5' },
+    6 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player6' },
+    7 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player7' },
+    8 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player8' },
+    9 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player9 '},
+    10 : { 'kd' : 0, 'dmg' : 0, 'winrate' : 0, 'kills' : 0, 'wins' : 0, 'totalDmg' : 0, 'assists' : 0, 'teamKills' : 0, 'score' : 0, 'revives' : 0, 'rounds' : 0, 'deaths' : 0, 'dbnos' : 0, 'top10s' : 0, 'name' : 'player10' }
 }
 
+numberOfPlayers = len(players.keys())
+
 # Populate PLAYERS dictionary with the season data
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['deaths'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['losses']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['assists'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['assists']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['teamKills'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['teamKills']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['revives'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['revives']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['kills'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['kills']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['wins'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['wins']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['totalDmg'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['damageDealt']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     players[i+1]['rounds'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['roundsPlayed']
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
+    players[i+1]['dbnos'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['dBNOs']
+    i += 1
+
+for i in range(0, numberOfPlayers):
+    players[i+1]['top10s'] = data['data'][i]['attributes']['gameModeStats']['squad-fpp']['top10s']
+    i += 1
+
+for i in range(0, numberOfPlayers):
     if players[i+1]['rounds'] == 0:
         players[i+1]['winrate'] = 0
     else:
         players[i+1]['winrate'] = round(float(players[i+1]['wins']) / float(players[i+1]['rounds']) * 100, 1)
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     if players[i+1]['rounds'] == 0:
         players[i+1]['dmg'] = 0
     else:
         players[i+1]['dmg'] = int(round(float(players[i+1]['totalDmg']) / float(players[i+1]['rounds'])))
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     if players[i+1]['rounds'] == 0:
         players[i+1]['kd'] = 0
     else:
         players[i+1]['kd'] = round(float(players[i+1]['kills']) / float(players[i+1]['deaths']), 2)
     i += 1
 
-for i in range(0, 10):
+for i in range(0, numberOfPlayers):
     if players[i+1]['rounds'] == 0:
         players[i+1]['score'] = 0
     else:
-        players[i+1]['score'] = round((float(players[i+1]['kills'] / 10) + float(players[i+1]['assists'] / 20) + float(players[i+1]['totalDmg'] / 1000) + float(players[i+1]['revives'] / 10) + float(players[i+1]['wins'] * 2) - float(players[i+1]['teamKills'] * 5)) * 100 / players[i+1]['rounds'], 1)
+        players[i+1]['score'] = round((float(players[i+1]['kills'] / 10) + float(players[i+1]['assists'] / 40) + float(players[i+1]['dbnos'] / 20) + float(players[i+1]['top10s'] / 20) + float(players[i+1]['totalDmg'] / 1000) + float(players[i+1]['revives'] / 10) + float(players[i+1]['wins'] * 2) - float(players[i+1]['teamKills'] / 2)) * 100 / players[i+1]['rounds'], 1)
     i += 1
 
 # Sort players by KD
 playersKd = sorted(players.items(), key = lambda x: x[1]['kd'], reverse=True)
 
-# Print multiple stats to STATS variable and print them to a file
+# Print all stats to STATS variable and print them to a file
 x = 0
 statsOutput = ''
 for i in playersKd:
     statsOutput += str(x+1) +': ' + playersKd[x][1]['name'] + ' ' + str(playersKd[x][1]['kd']) + ' kd, ' + str(playersKd[x][1]['dmg']) + ' dmg, ' + str(playersKd[x][1]['winrate']) + ' winrate. '
     x += 1
 
-with open('stats', 'w') as f:
+with open('homedirectory/stats', 'w') as f:
     print(statsOutput, file=f)
 
 # Print KD to KD variable and print them to a file
@@ -132,7 +142,7 @@ for i in playersKd:
     kdOutput += str(x+1) +': ' + playersKd[x][1]['name'] + ' ' + str(playersKd[x][1]['kd']) + ' '
     x += 1
 
-with open('kd', 'w') as f:
+with open('homedirectory/kd', 'w') as f:
     print(kdOutput, file=f)
 
 # Sort players by damage
@@ -145,7 +155,7 @@ for i in playersKd:
      dmgOutput += str(x+1) +': ' + playersDmg[x][1]['name'] + ' ' + str(playersDmg[x][1]['dmg']) + ' '
      x += 1
 
-with open('damage', 'w') as f:
+with open('homedirectory/damage', 'w') as f:
     print(dmgOutput, file=f)
 
 # Sort players by winning rate
@@ -158,7 +168,7 @@ for i in playersKd:
     winrateOutput += str(x+1) +': ' + playersWinrate[x][1]['name'] + ' ' + str(playersWinrate[x][1]['winrate']) + '% '
     x += 1
 
-with open('winrate', 'w') as f:
+with open('homedirectory/winrate', 'w') as f:
     print(winrateOutput, file=f)
 
 # Sort players by points
@@ -171,39 +181,39 @@ for i in playersKd:
     scoreOutput += str(x+1) +': ' + playersscore[x][1]['name'] + ' ' + str(playersscore[x][1]['score']) + ' '
     x += 1
 
-with open('score', 'w') as f:
+with open('homedirectory/score', 'w') as f:
     print(scoreOutput, file=f)
 
-# Print today's streamer chicken dinners to a file, change filenames and use absolute paths if necessary
-winsStreamer1_output = ''
-if winsStreamer1 == 0:
-    winsStreamer1_output = 'No chicken dinners today :('
-if winsStreamer1 == 1:
-    winsStreamer1_output = 'One chicken dinner today!'
-if winsStreamer1 > 1:
-    winsStreamer1_output = winsStreamer1_output + ' chicken dinners today!'
+# Print today's streamer chicken dinners to a file
+wins_streamer1_output = ''
+if wins_streamer1 == 0:
+    wins_streamer1_output = 'No chicken dinners tonight.'
+if wins_streamer1 == 1:
+    wins_streamer1_output = 'One chicken dinner tonight!'
+if wins_streamer1 > 1:
+    wins_streamer1_output = str(wins_streamer1) + ' chicken dinners tonight!'
 
-with open('stream1wins', 'w') as f:
-    print(winsStreamer1_output, file=f)
+with open('homedirectory/streamer1wins', 'w') as f:
+    print(wins_streamer1_output, file=f)
 
-winsStreamer2_output = ''
-if winsStreamer2 == 0:
-    winsStreamer2_output = 'No chicken dinners today :('
-if winsStreamer2 == 1:
-    winsStreamer2_output = 'One chicken dinner today!'
-if winsStreamer2 > 1:
-    winsStreamer2_output = winsStreamer2_output + ' chicken dinners today!'
+wins_streamer2_output = ''
+if wins_streamer2 == 0:
+    wins_streamer2_output = 'No chicken dinners tonight.'
+if wins_streamer2 == 1:
+    wins_streamer2_output = 'One chicken dinner tonight!'
+if wins_streamer2 > 1:
+    wins_streamer2_output = str(wins_streamer2) + ' chicken dinners tonight!'
 
-with open('streamer2wins', 'w') as f:
-    print(winsStreamer2_output, file=f)
+with open('homedirectory/streamer2wins', 'w') as f:
+    print(wins_streamer2_output, file=f)
 
-winsStreamer3_output = ''
-if winsStreamer3 == 0:
-    winsStreamer3_output = 'No chicken dinners today :('
-if winsStreamer3 == 1:
-    winsStreamer3_output = 'One chicken dinner today!'
-if winsStreamer3 > 1:
-    winsStreamer3_output = winsStreamer3_output + ' chicken dinners today!'
+wins_streamer3_output = ''
+if wins_streamer3 == 0:
+    wins_streamer3_output = 'No chicken dinners tonight.'
+if wins_streamer3 == 1:
+    wins_streamer3_output = 'One chicken dinner tonight!'
+if wins_streamer3 > 1:
+    wins_streamer3_output = str(wins_streamer3) + ' chicken dinners tonight!'
 
-with open('streamer3wins', 'w') as f:
-    print(winsStreamer3_output, file=f)
+with open('homedirectory/streamer3wins', 'w') as f:
+    print(wins_streamer3_output, file=f)
